64d5bc7843406c7bae71556096572b0724dba843
Dale Willemse <DaleWillemse000@gmail.com>
2022-11-01 14:50:00 +0200

    Add files via upload

---
+++ streamWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace streamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\DaleWillemse\Desktop\text.txt";
+            
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine("This is academic year 2022");
+                string text = "Welcome to Belgium Campus";
+
+                writer.WriteLine(text);
+                writer.Write("No more covid-19");
+                writer.Write("...yeeahh");
+            }
+
+            Console.WriteLine("Successfully written to file.");
+        }
+    }
+}
