c4bfd51343f6b43ebd8cc4bc3b8e522c0c6489a3
Dale Willemse <DaleWillemse000@gmail.com>
2022-11-01 13:53:40 +0200

    Add files via upload

---
+++ classes/classes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace classes
+{
+    internal class Program
+    {
+        public delegate bool checkMethod(string[] data, string name);
+
+        static void Main(string[] args)
+        {
+            
+            string[] data = {"Llewelyn", "Miguel", "Juanro", "Comfort", "Dale"};
+            
+            string name = " Dale ";
+            
+            checkMethod check = new checkMethod(search);
+            
+            if (check.Invoke(data, name.Trim())) // trim removes all spaces.
+            {
+                Console.WriteLine("Name found.");
+            }
+            else
+            {
+                Console.WriteLine("Name not found.");
+                
+            }
+            
+            Student student = new Student("Dale", 21, 123456);
+            student.displayInfo();
+        }
+        
+        public static bool search(string[] data, string name)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].Equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ classes/classes/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Student
+    {
+
+        private string name;
+        private int age;
+        private int id;
+        
+        public Student(string name, int age, int id)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+            Console.WriteLine("\nStudent created!");
+            
+        }
+
+        public void displayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Id: " + id);
+        }
+    }
+}
