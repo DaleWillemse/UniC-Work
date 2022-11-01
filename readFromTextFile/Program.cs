9bcc677381caf1dc0182669d6f947b05a43c84cb
Dale Willemse <DaleWillemse000@gmail.com>
2022-11-01 19:48:16 +0200

    Add files via upload

---
+++ readFromTextFile/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace readFromTextFile
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\DaleWillemse\Desktop\infor.txt";
+
+            readInfo(fileName);
+        }
+
+        public static void readInfo(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            
+            Console.WriteLine("Formatted Data\n================================================");
+            foreach(string line in lines)
+            {
+                string[] data = line.Split(',');
+                Console.WriteLine("First Name: {0}| Last Name: {1}| Job: {2}", data[0], data[1], data[2]);
+            }
+            Console.WriteLine("==================================================");
+        }
+    }
+}
