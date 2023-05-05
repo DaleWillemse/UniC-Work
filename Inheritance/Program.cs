0bdbce343e86b48a19960f17c8f171a3584d437e
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-05 22:02:06 +0200

    .

---
+++ Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿class Vehicle  // base class  
+{
+    public string brand = "Ford";  // Vehicle field
+
+}
+
+class Car : Vehicle  // derived class
+{
+    public string modelName = "Mustang";
+    public void hoot()             // Vehicle method 
+    {
+        Console.WriteLine("Beep beep.");
+    }
+}
+
+class Plane : Vehicle
+{
+    public string modelName = "Tri-Motor";
+    public void rotor()
+    {
+        Console.WriteLine("RrrRrrr");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.hoot();
+        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+
+        Plane myPlane = new Plane();
+        myPlane.rotor();
+        Console.WriteLine(myPlane.modelName);
+
+    }
+}
