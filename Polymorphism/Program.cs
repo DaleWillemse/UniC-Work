4ac985984078517ef16ec1d950e7ece0f1e2c07b
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-07 23:09:41 +0200

    .

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Polymorphism
+{
+
+    public class Shapes
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape.");
+        }
+    }
+
+    public class Circle : Shapes
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle.");
+        }
+    }
+
+    public class Square : Shapes
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a square.");
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle();
+        circle.Draw();
+
+        Shapes shape = new Shapes();
+        shape.Draw();
+    }
+}
